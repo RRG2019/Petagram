d2d425e8d73be9b94f6e9675cace5d73eb0760f8
RRG <r.rovira08@gmail.com>
2020-09-25 22:43:24 -0300

    Refresh Indicator

--- app/src/main/java/com/example/petagram/MainActivity.java
+++ app/src/main/java/com/example/petagram/MainActivity.java
@@ -1,22 +1,49 @@
 package com.example.petagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout srlMiIndicadorRefresh;
+    ListView lvMiLista;
+    Adapter adaptador;
+
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        srlMiIndicadorRefresh = findViewById(R.id.srlMiIndicadorRefresh);
+        lvMiLista = findViewById(R.id.lvMilista);
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+
+        lvMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        srlMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+    }
+
+    public void refrescandoContenido() {
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lvMiLista.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planetas));
+        srlMiIndicadorRefresh.setRefreshing(false);
     }
 
     public void agregarFAB(){
